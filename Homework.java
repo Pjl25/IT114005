ae93b6a4126b9fa737bb3301448a7dba8803b9df
Pjl25 <70656915+Pjl25@users.noreply.github.com>
2020-09-14 00:56:44 -0400

    Homework added

---
+++ Homework.java
@@ -0,0 +1,12 @@
+public class Homework {
+   public static void main(String[] args) {
+      //initializing all required variables
+      String name = new String ("Paul");
+      int age = 19;
+      double test = 3.98945632;
+     
+      
+      // outputting all required variables
+      System.out.println("name is " +name + ", age is " + age + ", test decimal is " + test);
+   }
+}
